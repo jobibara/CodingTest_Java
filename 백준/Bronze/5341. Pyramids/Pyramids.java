0bb39f7807fb6417b28558e30262b1299d9d8fb0
Jobibara <162455046+jobibara@users.noreply.github.com>
2025-04-12 14:21:44 +0900

    [Bronze V] Title: Pyramids, Time: 204 ms, Memory: 17312 KB -BaekjoonHub

---
+++ 백준/Bronze/5341. Pyramids/Pyramids.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		while(true) {
+			int n = sc.nextInt();
+			
+			if(n == 0) {
+				break;
+			}
+			int result = 0;
+			for(int i = n; i>0; i--) {
+				result += i;
+			}
+			
+			System.out.println(result);
+		}
+		
+	}
+}
