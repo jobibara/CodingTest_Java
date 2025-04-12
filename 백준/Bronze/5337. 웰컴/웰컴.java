fcda21f97b36b1ca020836ec5b80376f9ac78c7a
Jobibara <162455046+jobibara@users.noreply.github.com>
2025-04-12 14:20:52 +0900

    [Bronze V] Title: 웰컴, Time: 124 ms, Memory: 13940 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        bw.write(".  .   ."+"\n");
+        bw.write("|  | _ | _. _ ._ _  _"+"\n");
+        bw.write("|/\\|(/.|(_.(_)[ | )(/."+"\n");
+        bw.flush();
+        bw.close();
+    }
+}
