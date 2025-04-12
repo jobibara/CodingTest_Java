d4c631c316aa325991232de126936a1ed66fc6c1
Jobibara <162455046+jobibara@users.noreply.github.com>
2025-04-12 20:08:42 +0900

    [Bronze V] Title: 큰 수 곱셈, Time: 6108 ms, Memory: 406744 KB -BaekjoonHub

---
+++ 백준/Bronze/13277. 큰 수 곱셈/큰 수 곱셈.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        BigInteger big1 = new BigInteger(st.nextToken());
+        BigInteger big2 = new BigInteger(st.nextToken());
+        
+        BigInteger multi = big1.multiply(big2);
+        
+        System.out.println(multi);
+        
+    }
+}
