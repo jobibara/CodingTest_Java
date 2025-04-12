ccc2fdb315e9744de07bd170ac745ca90022e84f
Jobibara <162455046+jobibara@users.noreply.github.com>
2025-04-12 20:16:32 +0900

    [Bronze V] Title: Copier, Time: 232 ms, Memory: 18492 KB -BaekjoonHub

---
+++ 백준/Bronze/26574. Copier/Copier.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		for(int i = 0; i<N; i++) {
+			String n = sc.next();
+			System.out.println(n+" "+n);
+		}
+	}
+}
