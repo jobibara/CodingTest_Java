093e496b10e31652110ae1ce0f1ee5718450746a
Jobibara <162455046+jobibara@users.noreply.github.com>
2025-04-12 20:14:57 +0900

    [Bronze V] Title: 자동완성, Time: 208 ms, Memory: 17584 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String str = sc.next();
+		
+		if(str.equals("N")) {
+			System.out.println("Naver D2");
+		}
+		else if(str.equals("n")) {
+			System.out.println("Naver D2");
+		}
+		else {
+			System.out.println("Naver Whale");
+		}
+	}
+}
