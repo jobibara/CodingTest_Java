fedd2b23f8148a59353f4e8fd156fd31d364b796
Jobibara <162455046+jobibara@users.noreply.github.com>
2025-04-12 14:15:32 +0900

    [Bronze V] Title: Shares, Time: 204 ms, Memory: 17548 KB -BaekjoonHub

---
+++ 백준/Bronze/3733. Shares/Shares.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+        while(sc.hasNext()) {
+            int N = sc.nextInt();
+			int S = sc.nextInt();
+			
+			System.out.println(S/(N+1));
+        }
+		
+		
+	}
+}
