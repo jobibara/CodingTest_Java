c8e791f490388b91c5b27e9041f34f20900cf9cc
Jobibara <162455046+jobibara@users.noreply.github.com>
2025-04-12 20:14:23 +0900

    [Bronze V] Title: Cupcake Party, Time: 204 ms, Memory: 17612 KB -BaekjoonHub

---
+++ 백준/Bronze/24568. Cupcake Party/Cupcake Party.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int R = sc.nextInt();
+		int S = sc.nextInt();
+		
+		System.out.println((R*8)+(S*3)-28);
+	}
+}
