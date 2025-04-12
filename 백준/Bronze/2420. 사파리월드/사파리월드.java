93daa8fd4ebfc82c13f408944702f4eea2b8fe0d
Jobibara <162455046+jobibara@users.noreply.github.com>
2025-04-12 12:27:58 +0900

    [Bronze V] Title: 사파리월드, Time: 124 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+        
+        long ans;
+        
+        ans = Math.abs(a-b);
+        
+        bw.write(Long.toString(ans));
+        
+        bw.flush();
+        bw.close();
+    }
+}
