4d89d301050faf47a8f1788556c5dcffa2d10ab5
Jobibara <162455046+jobibara@users.noreply.github.com>
2025-04-12 20:18:54 +0900

    [Bronze V] Title: 정보섬의 대중교통, Time: 180 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/28113. 정보섬의 대중교통/정보섬의 대중교통.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		
+		if(A > B) {
+			System.out.println("Subway");
+		}
+		else if(A < B) {
+			System.out.println("Bus");
+		}
+		else {
+			System.out.println("Anything");
+		}
+	}
+
+}
