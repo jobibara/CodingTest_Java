3a57a777f468423ef54a70a80d84930436f61d6a
Jobibara <162455046+jobibara@users.noreply.github.com>
2025-04-15 01:22:24 +0900

    [Bronze V] Title: 라면 공식, Time: 136 ms, Memory: 14336 KB -BaekjoonHub

---
+++ 백준/Bronze/30007. 라면 공식/라면 공식.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st;
+		
+		for(int i = 0; i<N; i++) {
+			st = new StringTokenizer(br.readLine());
+			
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			int X = Integer.parseInt(st.nextToken());
+			
+			int W = A*(X-1) + B;
+			
+			System.out.println(W);
+		}
+	}
+}
