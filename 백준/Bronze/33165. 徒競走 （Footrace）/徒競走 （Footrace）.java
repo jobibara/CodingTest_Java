fcf829bd4cb7ced8bf561409ca56e3b0ec5120d9
Jobibara <162455046+jobibara@users.noreply.github.com>
2025-04-15 01:26:35 +0900

    [Bronze V] Title: 徒競走 (Footrace), Time: 172 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/33165. 徒競走 （Footrace）/徒競走 （Footrace）.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println(sc.nextInt()*sc.nextInt());
+		sc.close();
+	}
+}
