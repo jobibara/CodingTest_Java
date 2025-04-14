de8fb8ec27422e57c974616c82e3429042e5b267
Jobibara <162455046+jobibara@users.noreply.github.com>
2025-04-15 01:24:09 +0900

    [Bronze V] Title: 飴の袋詰め (Drops Packing), Time: 120 ms, Memory: 14360 KB -BaekjoonHub

---
+++ 백준/Bronze/31610. 飴の袋詰め （Drops Packing）/飴の袋詰め （Drops Packing）.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int result = Integer.parseInt(br.readLine())*Integer.parseInt(br.readLine()) + Integer.parseInt(br.readLine());
+		bw.write(Integer.toString(result));
+		bw.flush();
+	}
+}
