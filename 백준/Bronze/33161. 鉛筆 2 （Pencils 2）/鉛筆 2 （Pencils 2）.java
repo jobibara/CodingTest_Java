3e6dcb6b049e6684fd46332c94ebcb9c3efc2dd9
Jobibara <162455046+jobibara@users.noreply.github.com>
2025-04-15 01:26:27 +0900

    [Bronze V] Title: 鉛筆 2 (Pencils 2), Time: 188 ms, Memory: 17656 KB -BaekjoonHub

---
+++ 백준/Bronze/33161. 鉛筆 2 （Pencils 2）/鉛筆 2 （Pencils 2）.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+		System.out.println(num/5);
+	}
+}
