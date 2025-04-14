b8de6c51a2de4ec87bc30bd936192773bdae708c
Jobibara <162455046+jobibara@users.noreply.github.com>
2025-04-15 01:23:52 +0900

    [Bronze V] Title: Everyone is a winner, Time: 128 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Bronze/31450. Everyone is a winner/Everyone is a winner.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int M = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		
+		if(M%K == 0) {
+			bw.write("Yes");
+		}
+		else {
+			bw.write("No");
+		}
+		
+		bw.flush();
+	}
+}
