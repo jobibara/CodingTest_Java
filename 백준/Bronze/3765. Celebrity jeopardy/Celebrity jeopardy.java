92140c9defb98c03a5794d3bc9a103b1bd536efe
Jobibara <162455046+jobibara@users.noreply.github.com>
2025-05-01 13:39:49 +0900

    [Bronze IV] Title: Celebrity jeopardy, Time: 124 ms, Memory: 14092 KB -BaekjoonHub

---
+++ 백준/Bronze/3765. Celebrity jeopardy/Celebrity jeopardy.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		while(true) {
+			String str = br.readLine();
+			if(str == null) {
+				break;
+			}
+			bw.write(str+"\n");
+		}
+		bw.flush();
+	}
+}
