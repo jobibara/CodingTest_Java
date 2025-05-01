56f4c3df6d0c0f3469ba3427e03bc7596b19d5e8
Jobibara <162455046+jobibara@users.noreply.github.com>
2025-05-01 13:44:48 +0900

    [Bronze IV] Title: 영수증, Time: 128 ms, Memory: 14004 KB -BaekjoonHub

---
+++ 백준/Bronze/5565. 영수증/영수증.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int sum = Integer.parseInt(br.readLine());
+        for(int i = 0; i<9; i++) {
+            sum -= Integer.parseInt(br.readLine()); 
+            
+        }
+        System.out.println(sum);
+    }
+}
