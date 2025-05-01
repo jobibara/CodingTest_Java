d1b618e63d682f51ab971aa74faf002a66a2f362
Jobibara <162455046+jobibara@users.noreply.github.com>
2025-05-01 13:39:26 +0900

    [Bronze IV] Title: R2, Time: 172 ms, Memory: 17688 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println(-1 * sc.nextInt() + sc.nextInt()*2);
+		
+		sc.close();
+	}
+}
